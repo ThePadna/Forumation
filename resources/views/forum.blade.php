6911ef8bd6c7a470d94229861514268d9fb2e258
Padna <coryf175@gmail.com>
2019-08-18 22:06:14 +0100

    Added basic forum style and imported data

--- resources/views/forum.blade.php
+++ resources/views/forum.blade.php
@@ -1,7 +1,21 @@
 <html>
 @foreach($threads as $thread)
-    <div>
-    <h1> {{ $thread->title }} </h1>
+    <div id="{{ $thread->id }}" class="thread">
+        <h1 class="thread-title"> {{ $thread->title }} </h1>
+        <h2 class="thread-op"> Submitted by {{ $thread->op }} </h2>
     </div>
 @endforeach
+<style>
+h1 {
+    color:#0000CD;
+}
+.thread-op {
+    font-style: italic;
+}
+.thread {
+    width: 50%;
+    height: 15%;
+    border: 1px solid navy;
+}
+</style>
 </html>
