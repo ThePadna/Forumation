abd525068b1394f082c2d5568e680e590fcfaead
Padna <coryf175@gmail.com>
2019-08-18 22:59:06 +0100

    Added 'posts' decimal to threads table, for sizing

--- resources/views/forum.blade.php
+++ resources/views/forum.blade.php
@@ -1,11 +1,17 @@
 <html>
 @foreach($threads as $thread)
-    <div id="{{ $thread->id }}" class="thread">
-        <h1 class="thread-title"> {{ $thread->title }} </h1>
-        <h2 class="thread-op"> Submitted by {{ $thread->op }} </h2>
+    <div id="{{ $thread->id }}" class="thread" style="height:{{$thread->posts}}%; width:{{$thread->posts}}%; background-color:gray;">
+        <h1 class="thread-title" style="font-size:{{$thread->posts}};"> {{ $thread->title }} </h1>
+        <h2 class="thread-op" style="font-size:{{$thread->posts / 2}};"> Submitted by {{ $thread->op }} </h2>
     </div>
 @endforeach
 <style>
+html, body {
+    height: 100%;
+    width: 100%;
+    border: 0;
+    margin: 0;
+}
 h1 {
     color:#0000CD;
 }
@@ -13,9 +19,7 @@ h1 {
     font-style: italic;
 }
 .thread {
-    width: 50%;
-    height: 15%;
-    border: 1px solid navy;
+    border: 1px solid red;
 }
 </style>
 </html>
