403659a1370c006edeff7a2a349b5acb7f68796b
Padna <coryf175@gmail.com>
2019-08-21 03:07:20 +0100

    Changed back to non-boxes, playing with some css

--- resources/views/forum.blade.php
+++ resources/views/forum.blade.php
@@ -1,36 +1,54 @@
 <html>
-<div id="container">
-<canvas id="threads" style="width:100%; height:500px;" />
+@foreach($threads as $t)
+<div id="wrapper">
+<div id="threads">
+<a href="/threads/{{$t->id}}">
+<div id="thread">
+<div id="title">
+<p> {{$t->title}} we dwr wwwdwdw dw wdwd wd wd wd w</p>
 </div>
-<script>
-var canv = document.getElementById("threads");
-var ctx = canv.getContext("2d");
-ctx.lineWidth = (40 / this.size);
-ctx.fillStyle = 'black;';
-ctx.fillRect(0, 0, 800, 800);
-</script>
+<div id="op">
+<p> Submitted by <span> {{$t->op}} </span> </p>
+</div>
+</div>
+</a>
+</div>
+</div>
+@endforeach
 <style>
-html, body {
-    height: 100%;
-    width: 100%;
-    border: 0;
-    margin: 0;
+span {
+  font-style: italic;
 }
-#container {
-  position: fixed;
-  left: 0;
-  top: 0;
-  width: 100vw;
-  height: 100vh;
-  background-color: white;
-  display: flex;
+body {
+  background-color:#cc9900;
 }
-#threads {
-  margin: auto;
-  width: 10em;
-  height: 5em;
+a {
+  text-decoration: none;
+}
+#wrapper {
+  text-align:center;
+  display:flex;
+  justify-content: center;
+}
+#op>p {
+  font-size: 10px;
+}
+#title>p {
+  font-size: 30px;
+}
+p {
+  color: black;
   background-color: white;
-  box-shadow: 1px 1px 10px rgba(0, 0, 0, .4);
+  border: 3px;
+  border-radius: 3px;
+  height: 20s%;
+  width: 500px;
+}
+#thread {
+  margin-top: 10px;
+  border: 5px solid black;
+  border-radius: 10px;
+  background-color:  #ffff66;
 }
 </style>
 </html>
