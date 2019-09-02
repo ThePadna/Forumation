a6a74996a29d52b75fe4327e4ea5e585ad406957
Padna <coryf175@gmail.com>
2019-09-02 16:47:38 +0100

    Added categories migration

--- database/migrations/2019_09_02_154059_create_categories_table.php
+++ database/migrations/2019_09_02_154059_create_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateThreadsTable extends Migration
+class CreateCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,9 @@ class CreateThreadsTable extends Migration
      */
     public function up()
     {
-        Schema::create('threads', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('op', 32)->unique();
-            $table->string('title', 300)->unique();
-            $table->decimal('categoryId');
-            $table->decimal("posts");
+            $table->string('name', 300);
             $table->timestamps();
         });
     }
@@ -30,6 +27,6 @@ class CreateThreadsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('threads');
+        Schema::dropIfExists('categories');
     }
 }
