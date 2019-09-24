6c4613023905d26f94c6840865670bab1bec0f8c
Padna <coryf175@gmail.com>
2019-09-24 20:12:25 +0100

    moved categoriesjs to public/js

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -24,87 +24,13 @@
             @endauth
     </div>
 </div>
-<script src="https://code.jquery.com/jquery-3.4.1.min.js"> </script>
 <form id="addCategoryForm">
 Input new Category name <br />
 <input id="categoryTitle" type="text" name="categoryname"/>
 <button id="categoryFormCloser"> Submit </button>
 </form>
 <button id="categoryFormOpener"> Add new Category </button>
-<script>
-var $form = $('#addCategoryForm');
-$form.hide();
-$form.submit((e) => {
-    e.preventDefault();
-    $.ajax({
-        type: "POST",
-        url: 'postcategory',
-        headers: {'X-CSRF-TOKEN': '{{csrf_token()}}'},
-        data: $form.serialize(),
-        success: function(res) {
-            console.log('Submission was successful.');
-            alert(res);
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-            console.log('An error occurred.');
-            console.log(errorThrown);
-            console.log(jqXHR);
-        },
-    });
-});
-var $categoryFormOpener = $('#categoryFormOpener').click(() => {
-    $form.show();
-    $categoryFormOpener.hide();
-});
-var dragged;
-document.addEventListener("dragstart", function(event) {
-  dragged = event.target;
-}, false);
-document.addEventListener("drop", function(event) {
-  event.preventDefault();
-  event.target.style.background = "white";
-  var node = event.target.nodeName;
-  var targetElement;
-  if(node.localeCompare("P") === 0) targetElement = event.target.parentElement.parentElement;
-  else if(node.localeCompare("DIV") == 0) targetElement = event.target.parentElement;
-  var targetInnerHTML = targetElement.innerHTML, targetHREF = targetElement.href;
-  targetElement.innerHTML = dragged.innerHTML;
-  dragged.innerHTML = targetInnerHTML
-  targetElement.href = dragged.href;
-  dragged.href = targetHREF;
-  console.log("Dragged: " + dragged.getAttribute("categoryId") + " target: " + targetElement.getAttribute("categoryId"));
-  $.ajax({
-        type: "POST",
-        url: 'categoryswitchid',
-        headers: {'X-CSRF-TOKEN' : '{{csrf_token()}}'},
-        data: {
-          'draggedId': targetElement.getAttribute("categoryId"),
-	        'targetId': dragged.getAttribute("categoryId")
-        },
-        success: function(res) {
-            alert(res);
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-            console.log('An error occurred.');
-            console.log(errorThrown);
-            console.log(jqXHR);
-        },
-    });
-}, false);
-document.addEventListener("dragover", function(event) {
-  event.preventDefault();
-}, false);
-document.addEventListener("dragenter", function(event) {
-  if (event.target.className == "drop-zone") {
-    event.target.style.background = "gray";
-  }
-
-}, false);
-document.addEventListener("dragleave", function(event) {
-  if (event.target.className == "drop-zone") {
-    event.target.style.background = "white";
-  }
-
-}, false);
-</script>
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"> </script>
+<meta name="csrf" content="{{csrf_token()}}">
+<script src="{{asset('js/categories.js')}}"> </script>
 @stop
