8a77437e3703e1134e542b0378bfec06d9564f4c
Padna <coryf175@gmail.com>
2019-10-14 17:46:03 +0100

    Added post thread button

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -2,6 +2,13 @@
 @section('content')
 <link rel="stylesheet" href="{{asset('css/category.css')}}">
 <div id="wrapper">
+    @auth 
+    <a href="postthread">
+    <div id="postThread">
+        <h1> Post a new thread </h1>
+    </div>
+</a>
+    @endauth
     <div id="threads">
         @foreach($threads as $t)
         <a href="/threads/{{$t->id}}">
@@ -10,20 +17,20 @@
                     <p> {{$t->title}}</p>
                 </div>
                 <div id="op">
-                    <p> Submitted by &nbsp;<span> <span class="glyphicon glyphicon-user"> </span> {{$t->op}} &nbsp;
-                            <span class="glyphicon glyphicon-fire">&nbsp;{{floor($t->posts)}}<span
-                                    class="glyphicon glyphicon-envelope"> </span> </span></i></p>
+                    <p> Submitted by &nbsp; <i class="far fa-user"></i> </p>
                 </div>
             </div>
         </a>
         @endforeach
     </div>
-    <a href="/{{$category->name}}/{{$page < 2 ? 1 : $page - 1}}">
+    @if($page > 1)
+    <a href="{{$page < 2 ? 1 : $page - 1}}">
         <div id="prevpage">
         <i class="fas fa-long-arrow-alt-left"></i>
         </div>
     </a>
-    <a href="/{{$category->name}}/{{$page + 1}}">
+    @endif
+    <a href="{{$page + 1}}">
         <div id="nextpage">
         <i class="fas fa-long-arrow-alt-right"></i>
         </div>
