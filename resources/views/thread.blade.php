e30fa33bd1338a81f24485a6ce75a74d32d7448f
Padna <coryf175@gmail.com>
2019-11-07 08:16:06 +0000

    Fixed spacing on thread posting UI

--- resources/views/thread.blade.php
+++ resources/views/thread.blade.php
@@ -5,7 +5,7 @@
     @foreach($posts as $p)
     <div id="container">
         <div class="row">
-            <div class="col-sm-2">
+            <div id="test" class="col-sm-2">
                 <p> {{App\User::find($p->user)->name}} </p>
             </div>
             <div class="col-sm-10">
@@ -16,8 +16,16 @@
     @endforeach
     @auth
     @if($isLastPage)
-    <textarea id="postReply" name="postReply"> </textarea>
+    <div id="replyForm">
+        <form>
+            <textarea id="replyText" name="replyText" placeholder="What are your thoughts?"></textarea>
+            <button id="postReply"> POST </button>
+        </form>
+    </div>
     @endif
     @endauth
 </div>
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"> </script>
+<meta name="csrf" content="{{csrf_token()}}">
+<script src="{{asset('js/thread.js')}}"> </script>
 @stop
