bcfdd46a7acb908df121b203e96438ff38e1855b
Padna <coryf175@gmail.com>
2019-11-13 06:51:35 +0000

    Changed sass files to make use of variables

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,7 +1,14 @@
 @extends('layouts/forum_layout')
 @section('content')
-<div id="profile_header">
+<link rel="stylesheet" href="{{asset('css/profile.css')}}">
+<div id="profileheader">
     <img src="{{asset('img/profilepic.png')}}" />
     <h1> {{$user->name}} </h1>
+</div>
+<div id="navwrapper">
+    <div id="nav">
+        <button id="postsBtn"> Posts </button>
+        <button id="threadsBtn"> Threads </button>
+    </div>
 </div>
 @stop
