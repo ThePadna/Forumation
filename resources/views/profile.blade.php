4c6ebdbdc3b1c6ed56f70c64d85981b2204c957a
Padna <coryf175@gmail.com>
2019-11-13 07:34:51 +0000

    Added css for layout of profile pages

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -5,10 +5,20 @@
     <img src="{{asset('img/profilepic.png')}}" />
     <h1> {{$user->name}} </h1>
 </div>
-<div id="navwrapper">
-    <div id="nav">
-        <button id="postsBtn"> Posts </button>
-        <button id="threadsBtn"> Threads </button>
+<div id="container">
+    <div class="row">
+        <div class="col-sm-4"> 
+            <h1 class="title"> Posts </h1>
+            <p class="counter"> 50 </p>
+        </div>
+        <div class="col-sm-4"> 
+            <h1 class="title"> Threads </h1>
+            <p class="counter"> 50 </p>
+        </div>
+        <div class="col-sm-4">
+            <h1 class="title"> Score </h1>
+            <p class="counter"> 50 </p>
+        </div>
     </div>
 </div>
 @stop
