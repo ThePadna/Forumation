72ab7f4515332b0816d68ad1ca1b4ef85178f586
Padna <coryf175@gmail.com>
2019-11-16 13:31:54 +0000

    UserController#editProfile now functional

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+use App\User;
+
 class UserController extends Controller
 {
+
+    /**
+     * Show profile editor page
+     * 
+     * 
+     */
+    public function editProfile(Request $request, $userId) {
+        $user = User::find($userId);
+        if($user == null) return view('404');
+        return view('profile/edit', ['user' => $user]);
+    }
 }
