d0c33ab654a42973a07198438a54c5612b3c8188
Padna <coryf175@gmail.com>
2019-12-20 10:54:07 +0000

    Finished implementing dynamic form rendering

--- public/js/categories.js
+++ public/js/categories.js
@@ -45,161 +45,233 @@ var delCategoryFormHTML = `<div id="delCategoryForm" class="popup-form">
 
 /**
  * Gain reference to forms and hide them in anticipation for button press.
+ * Initialize them with #initForms before submission
  */
-var $addCategoryForm = $('#addCategoryForm'), $delCategoryForm = $('#delCategoryForm'), $editCategoryForm = $('#editCategoryForm');
-
+var $addCategoryForm,
+    $delCategoryForm,
+    $editCategoryForm;
+function initForms() {
+    $addCategoryForm = $('#addCategoryForm');
+    $delCategoryForm = $('#delCategoryForm');
+    $editCategoryForm = $('#editCategoryForm');
+}
 /**
+ * Listen for form submission
  * Request to post new category when form is submitted.
  */
-$addCategoryForm.submit((e) => {
-    e.preventDefault();
-    $title = $addCategoryForm.find('input[name="categoryTitle"]').val();
-    $addCategoryForm.find('input[name="categoryTitle"]').val("");
-    $.ajax({
-      type: "POST",
-      url: '/postcategory',
-      headers: {'X-CSRF-TOKEN' : $('meta[name="csrf"]').attr('content')},
-      data: {"categoryTitle": $title},
-      success: function(res) {
-        window.location.reload();
-        console.log(res);
-      },
-      error: function(xhr, ajaxOptions, thrownError) {
-        console.log("Error occured during AJAX request, error code: " + xhr.status);
-      },
+function registerAddFormSubmitListener() {
+    $addCategoryForm.submit(e => {
+        console.log("umit");
+        e.preventDefault();
+        $title = $addCategoryForm.find('input[name="categoryTitle"]').val();
+        $addCategoryForm.find('input[name="categoryTitle"]').val("");
+        $.ajax({
+            type: "POST",
+            url: "/postcategory",
+            headers: { "X-CSRF-TOKEN": $('meta[name="csrf"]').attr("content") },
+            data: { categoryTitle: $title },
+            success: function(res) {
+                window.location.reload();
+            },
+            error: function(xhr, ajaxOptions, thrownError) {
+                console.log(
+                    "Error occured during AJAX request, error code: " +
+                        xhr.status
+                );
+            }
+        });
     });
-});
+}
 /**
+ * Listen for form submission
  * Request to delete category when form is submitted.
  */
-$delCategoryForm.submit((e) => {
-  e.preventDefault();
-  $.ajax({
-      type: "POST",
-      url: '/delcategory',
-      headers: {'X-CSRF-TOKEN': $('meta[name="csrf"]').attr('content')},
-      data: {'categoryName': this.$prevClickedDelCategoryName},
-      success: function(res) {
-        window.location.reload();
-        console.log(res);
-      },
-      error: function(xhr, ajaxOptions, thrownError) {
-        console.log("Error occured during AJAX request, error code: " + xhr.status);
-      },
-  });
-});
+function registerDelFormSubmitListener() {
+    $delCategoryForm.submit(e => {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "/delcategory",
+            headers: { "X-CSRF-TOKEN": $('meta[name="csrf"]').attr("content") },
+            data: { categoryName: this.$prevClickedDelCategoryName },
+            success: function(res) {
+                window.location.reload();
+            },
+            error: function(xhr, ajaxOptions, thrownError) {
+                console.log(
+                    "Error occured during AJAX request, error code: " +
+                        xhr.status
+                );
+            }
+        });
+    });
+}
 
 /**
+ * Listen for form submission
  * Request to edit category when form is submitted.
  */
-$editCategoryForm.submit((e) => {
-  e.preventDefault();
-  $categoryName = $editCategoryForm.find('input[name="categoryname"]').val();
-  $.ajax({
-      type: "POST",
-      url: '/editcategory',
-      headers: {'X-CSRF-TOKEN': $('meta[name="csrf"]').attr('content')},
-      data: {'categoryName': this.$prevClickedEditCategoryName, 'newCategoryName': $categoryName},
-      success: function(res) {
-        window.location.reload();
-      },
-      error: function(xhr, ajaxOptions, thrownError) {
-        console.log("Error occured during AJAX request, error code: " + xhr.status);
-      },
-  });
-});
+function registerEditFormSubmitListener() {
+    $editCategoryForm.submit(e => {
+        e.preventDefault();
+        $categoryName = $editCategoryForm
+            .find('input[name="categoryname"]')
+            .val();
+        $.ajax({
+            type: "POST",
+            url: "/editcategory",
+            headers: { "X-CSRF-TOKEN": $('meta[name="csrf"]').attr("content") },
+            data: {
+                categoryName: this.$prevClickedEditCategoryName,
+                newCategoryName: $categoryName
+            },
+            success: function(res) {
+                window.location.reload();
+            },
+            error: function(xhr, ajaxOptions, thrownError) {
+                console.log(
+                    "Error occured during AJAX request, error code: " +
+                        xhr.status
+                );
+            }
+        });
+    });
+}
 
 /**
  * Open category creator form on button press.
  */
-$('#categoryFormOpener').on('click', (e) => {
-    $('#categories').append(addCategoryFormHTML);
+$("#categoryFormOpener").on("click", e => {
+    $("#categories").append(addCategoryFormHTML);
     this.registerFormExitHandler();
+    this.initForms();
+    this.registerAddFormSubmitListener();
 });
 
 /**
  * Acquire dragged element in object for use in future events.
  */
 var dragged;
-document.addEventListener("dragstart", function(event) {
-  dragged = event.target;
-}, false);
+document.addEventListener(
+    "dragstart",
+    function(event) {
+        dragged = event.target;
+    },
+    false
+);
 
 /**
  * Request to switch category IDs when category dropped on another category.
  */
-document.addEventListener("drop", function(event) {
-  event.preventDefault();
-  if(event.target.className == "drop-zone") {
-    event.target.style.background = "white";
-    var node = event.target.nodeName;
-    var targetElement;
-    if(node.localeCompare("P") === 0) targetElement = event.target.parentElement.parentElement;
-    else if(node.localeCompare("DIV") == 0) targetElement = event.target.parentElement;
-    var targetInnerHTML = targetElement.innerHTML, targetHREF = targetElement.href;
-    targetElement.innerHTML = dragged.innerHTML;
-    dragged.innerHTML = targetInnerHTML
-    targetElement.href = dragged.href;
-    dragged.href = targetHREF;
-    $.ajax({
-      type: "POST",
-      url: '/categoryswitchid',
-      headers: {'X-CSRF-TOKEN' : $('meta[name="csrf"]').attr('content')},
-      data: {
-        'draggedId': targetElement.getAttribute("categoryId"),
-        'targetId': dragged.getAttribute("categoryId")
-      },
-      error: function(xhr, ajaxOptions, thrownError) {
-        console.log("Error occured during AJAX request, error code: " + xhr.status);
-      },
-    });
-  }
-}, false);
+document.addEventListener(
+    "drop",
+    function(event) {
+        event.preventDefault();
+        if (event.target.className == "drop-zone") {
+            event.target.style.background = "white";
+            var node = event.target.nodeName;
+            var targetElement;
+            if (node.localeCompare("P") === 0)
+                targetElement = event.target.parentElement.parentElement;
+            else if (node.localeCompare("DIV") == 0)
+                targetElement = event.target.parentElement;
+            var targetInnerHTML = targetElement.innerHTML,
+                targetHREF = targetElement.href;
+            targetElement.innerHTML = dragged.innerHTML;
+            dragged.innerHTML = targetInnerHTML;
+            targetElement.href = dragged.href;
+            dragged.href = targetHREF;
+            $.ajax({
+                type: "POST",
+                url: "/categoryswitchid",
+                headers: {
+                    "X-CSRF-TOKEN": $('meta[name="csrf"]').attr("content")
+                },
+                data: {
+                    draggedId: targetElement.getAttribute("categoryId"),
+                    targetId: dragged.getAttribute("categoryId")
+                },
+                error: function(xhr, ajaxOptions, thrownError) {
+                    console.log(
+                        "Error occured during AJAX request, error code: " +
+                            xhr.status
+                    );
+                }
+            });
+        }
+    },
+    false
+);
 
 /**
  * Set target zone to distinguishable colour on enter.
  */
-document.addEventListener("dragenter", function(event) {
-  if(event.target.className == "drop-zone") {
-    event.target.style.background = "black";
-  }
-
-}, false);
+document.addEventListener(
+    "dragenter",
+    function(event) {
+        if (event.target.className == "drop-zone") {
+            event.target.style.background = "black";
+        }
+    },
+    false
+);
 
 /**
  * Set target zone back to original colour after leaving.
  */
-document.addEventListener("dragleave", function(event) {
-  if(event.target.className == "drop-zone") {
-    event.target.style.background = "white";
-  }
-}, false);
+document.addEventListener(
+    "dragleave",
+    function(event) {
+        if (event.target.className == "drop-zone") {
+            event.target.style.background = "white";
+        }
+    },
+    false
+);
 
 /**
  * Register click handler every time we append form to DOM.
  */
 function registerFormExitHandler() {
-  $('.form-exit').on('click', (e) => {
-    $('.popup-form').remove();
-  });
+    $(".form-exit").on("click", e => {
+        $(".popup-form").remove();
+    });
 }
 /**
  * Set file scope var for use in ajax request.
  * Replace placeholder value with clicked category name.
  */
-$('.del-category').on('click', (e) => {
-  e.preventDefault();
-  this.$prevClickedDelCategoryName = $(e.target).parent().children()[0].innerHTML;
-  $('#categories').append(delCategoryFormHTML.replace("%c", this.$prevClickedDelCategoryName.trim()));
-  this.registerFormExitHandler();
+$(".del-category").on("click", e => {
+    e.preventDefault();
+    this.$prevClickedDelCategoryName = $(e.target)
+        .parent()
+        .children()[0].innerHTML;
+    $("#categories").append(
+        delCategoryFormHTML.replace(
+            "%c",
+            this.$prevClickedDelCategoryName.trim()
+        )
+    );
+    this.registerFormExitHandler();
+    this.initForms();
+    this.registerDelFormSubmitListener();
 });
 
 /**
  * Fill form's input with old category ready to edit.
  */
-$('.edit-category').on('click', (e) => {
-  e.preventDefault();
-  this.$prevClickedDelCategoryName = $(e.target).parent().children()[0].innerHTML;
-  $('#categories').append(editCategoryFormHTML.replace("%c", this.$prevClickedDelCategoryName.trim()));
-  this.registerFormExitHandler();
+$(".edit-category").on("click", e => {
+    e.preventDefault();
+    this.$prevClickedDelCategoryName = $(e.target)
+        .parent()
+        .children()[0].innerHTML;
+    $("#categories").append(
+        editCategoryFormHTML.replace(
+            "%c",
+            this.$prevClickedDelCategoryName.trim()
+        )
+    );
+    this.registerFormExitHandler();
+    this.initForms();
+    this.registerEditFormSubmitListener();
 });
