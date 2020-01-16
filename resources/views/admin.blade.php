f07e5a65814d9bf43b0c6d3669347eb2e1a10105
Padna <coryf175@gmail.com>
2020-01-16 23:47:22 +0000

    playing around with switches for #settings

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,31 +1,35 @@
 <html>
 
 <head>
-<link rel="stylesheet" href="{{asset('css/admin.css')}}">
+    <link rel="stylesheet" href="{{asset('css/admin.css')}}">
 </head>
 <div id="container">
     <div id="settings">
         <div id="color-scheme">
             <h1> Color Scheme </h1>
+                <label class="switch">
+                    <input type="checkbox" />
+                    <div></div>
+                </label>
             <!-- slide btn -->
         </div>
     </div>
     <div id="menu" class="row">
-            <div class="col-sm-4">
-                <a href="">
-                    <p> Users </p>
-                </a>
-            </div>
-            <div class="col-sm-4">
-                <a href="">
-                    <p> Threads </p>
-                </a>
-            </div>
-            <div class="col-sm-4">
-                <a href="">
-                    <p> Purge </p>
-                </a>
-            </div>
+        <div class="col-sm-4">
+            <a href="">
+                <p> Users </p>
+            </a>
+        </div>
+        <div class="col-sm-4">
+            <a href="">
+                <p> Threads </p>
+            </a>
+        </div>
+        <div class="col-sm-4">
+            <a href="">
+                <p> Purge </p>
+            </a>
+        </div>
     </div>
 </div>
 
