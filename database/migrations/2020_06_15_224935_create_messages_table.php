577d4bc0f90dd100350e88db4e76f85448c54707
Padna <coryf175@gmail.com>
2020-06-15 23:55:04 +0100

    Added migration for messages

---
+++ database/migrations/2020_06_15_224935_create_messages_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->string("contents", 5000);
+            $table->integer("recipient");
+            $table->integer("sender");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
